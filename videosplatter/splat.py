1e952845e7647f1168edce7849c69f73781afcf1
Ryan Fox <ryan@foxrow.com>
2022-02-09 21:07:18 -0600

    add stats mode

--- videosplatter/splat.py
+++ videosplatter/splat.py
@@ -1,4 +1,5 @@
 import argparse
+import io
 import math
 import os
 import re
@@ -33,6 +34,28 @@ def get_color(image):
     return centers[0].reshape((-1, 3))
 
 
+def get_colorfulness(image):
+    """As defined by Hasler and Süsstrunk, 2003"""
+    b, g, r = cv2.split(image.astype("float"))
+    rg = np.absolute(r - g)
+    rg_mean = np.mean(rg)
+    rg_std_dev = np.std(rg)
+
+    yb = np.absolute(0.5 * (r + g) - b)
+    yb_mean = np.mean(yb)
+    yb_std_dev = np.std(yb)
+
+    std_root = np.sqrt((rg_std_dev ** 2) + (yb_std_dev ** 2))
+    mean_root = np.sqrt((rg_mean ** 2) + (yb_mean ** 2))
+
+    return std_root + (0.3 * mean_root)
+
+
+def get_contrast(image):
+    """RMS contrast"""
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY).std()
+
+
 def barcode(filename, start, end, interval):
     clip = VideoFileClip(filename)
     end = end or int(clip.duration)
@@ -59,6 +82,27 @@ def splat(filename, start, end, interval):
     subprocess.run([FFMPEG_BINARY, '-ss', str(start), '-i', filename, '-frames', '1', '-vf', filter_string, output_filename])
 
 
+def stats(filename, start, end, interval):
+    clip = VideoFileClip(filename)
+    end = end or int(clip.duration)
+
+    colorfulnesses = []
+    contrasts = []
+
+    for timestamp in range(start, end, interval):
+        frame = clip.get_frame(timestamp)
+        colorfulnesses.append(get_colorfulness(frame))
+        contrasts.append(get_contrast(frame))
+
+    colors = np.asarray(colorfulnesses)
+    cons = np.asarray(contrasts)
+
+    # mean, median, std dev for colorfulness and contrast
+    return f"""\t\t\tmean\tmedian\tstd dev
+color\t\t{colors.mean(): >6.2f}\t{np.median(colors): >6.2f}\t{colors.std(): >6.2f}
+contrast\t{cons.mean(): >6.2f}\t{np.median(cons): >6.2f}\t{cons.std(): >6.2f}"""
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('file', help='video file to splatter')
@@ -66,12 +110,15 @@ def main():
     parser.add_argument('--end', type=int, default=None, help='end time in seconds')
     parser.add_argument('-i', '--interval', type=int, default=10, help='how many seconds between images')
     parser.add_argument('-b', '--barcode', action='store_true', help='barcode mode. create a "barcode" of all the colors in the video')
+    parser.add_argument('-s', '--stats', action='store_true', help='caculate contrast and colorfulness stats')
 
     args = parser.parse_args()
 
     if args.barcode:
         image = barcode(args.file, args.start, args.end, args.interval)
         image.save(os.path.splitext(args.file)[0] + '_barcode.jpg')
+    elif args.stats:
+        print(stats(args.file, args.start, args.end, args.interval))
     else:
         splat(args.file, args.start, args.end, args.interval)
 
